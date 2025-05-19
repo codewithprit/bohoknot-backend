cc757f8043729d06b42f1fff43df422130e6b794
pritesh8329 <101930978+codewithprit@users.noreply.github.com>
2025-05-19 22:37:51 +0530

    first commit
    first commit

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    //
+
+    public function greet(){
+        print("bkl");exit;
+        // return response->json([
+        //     "message" => "hello"
+        // ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+use App\Http\Controllers\ApiController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get("/api/greet", [ApiController::class, "greet"]);
