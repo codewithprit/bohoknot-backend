4ecaea2598f42cd4d2930d92d91fb714bbe39471
pritesh8329 <101930978+codewithprit@users.noreply.github.com>
2025-11-18 10:25:39 +0530

    latest changes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,24 @@
 <?php
 use App\Http\Controllers\ApiController;
-use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Web\AuthWebController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get("/api/greet", [ApiController::class, "greet"]);
+// Route::get("/api/greet", [ApiController::class, "greet"]);
 // Route::middleware('api')->prefix('api')->group(function(){
 
 //     Route::post('/register', [RegisterController::class, "register"]);
 //     Route::post('/login', [LoginController::class, "login"]);
 // });
+
+
+Route::get("/login", [AuthWebController::class, "showLoginForm"]);
+Route::get("/register", [AuthWebController::class, "showRegisterForm"]);
+Route::get("/otp", [AuthWebController::class, "showOtpform"]);
+
+
 
 
